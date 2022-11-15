89065d8f90bb640d908091a2417deeb02ccc08dc
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-11-15 16:09:18 -0500

    import json file in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import swaggerUi from 'swagger-ui-express';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import apiRoutes from './app.routes';
+import swaggerDocument from '../../swagger.json';
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', apiRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
